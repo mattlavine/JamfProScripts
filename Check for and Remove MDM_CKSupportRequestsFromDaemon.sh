5913ad7fd952a7fffe63f9f8fcbf6c4ff5e16fc8
mattlavine <mattlavine@icloud.com>
2017-08-10 15:32:03 -0400

    Added Exits

--- Check for and Remove MDM_CKSupportRequestsFromDaemon.sh
+++ Check for and Remove MDM_CKSupportRequestsFromDaemon.sh
@@ -2,13 +2,19 @@
 
 if [ -f /var/db/MDM_CKSupportRequestsFromDaemon ]; then
 	echo "File exists."
-	echo "Deleting file."
+	echo "Attempting to delete file..."
 	rm -f /var/db/MDM_CKSupportRequestsFromDaemon
 	if [ -f /var/db/MDM_CKSupportRequestsFromDaemon ]; then
-		echo "File was not deleted..."
+		echo "File was not deleted."
+		echo "Quitting with Error 1"
+		exit 1
 	else
-		echo "File was deleted..."
+		echo "File was deleted."
+		echo "Quitting."
+		exit 0
 	fi
 else
 	echo "File doesn't exist."
+	echo "Quitting."
+	exit 0
 fi
