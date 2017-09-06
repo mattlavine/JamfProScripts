f0349fde0598ad84b0a5f16445bb48a71f8724f4
mattlavine <mattlavine@icloud.com>
2017-09-06 12:31:21 -0400

    More App Shortcuts

--- First Boot.sh
+++ First Boot.sh
@@ -2,23 +2,72 @@
 
 # Define variables
 ln="/bin/ln"
-rm="/bin/rm"
 
 #####################################################
 # \# \# Baseline initial System Setup \#
 # \#
 #####################################################
 
-# Make a shortcut links to Network Utility, Directory Utility, Screen Sharing, Raid Utility, and Archive Utility under "Utilities" Folder
-ln -s /System/Library/CoreServices/Applications/Network\ Utility.app /Applications/Utilities/Network\ Utility.app
-ln -s /System/Library/CoreServices/Applications/Directory\ Utility.app /Applications/Utilities/Directory\ Utility.app
-ln -s /System/Library/CoreServices/Applications/Screen\ Sharing.app /Applications/Utilities/Screen\ Sharing.app
-ln -s /System/Library/CoreServices/Applications/RAID\ Utility.app /Applications/Utilities/RAID\ Utility.app
-ln -s /System/Library/CoreServices/Applications/Archive\ Utility.app /Applications/Utilities/Archive\ Utility.app
-ln -s /System/Library/CoreServices/Applications/About\ This\ Mac.app /Applications/Utilities/About\ This\ Mac.app
-ln -s /System/Library/CoreServices/Applications/Storage\ Management.app /Applications/Utilities/Storage\ Management.app
-ln -s /System/Library/CoreServices/Software\ Update.app /Applications/Utilities/Software\ Update.app
-ln -s /System/Library/CoreServices/Ticket\ Viewer.app /Applications/Utilities/Ticket\ Viewer.app
-ln -s /System/Library/CoreServices/Siri.app /Applications/Utilities/Siri.app
-ln -s /System/Library/CoreServices/Certificate\ Assistant.app /Applications/Utilities/Certificate\ Assistant.app
-ln -s /System/Library/CoreServices/Applications/System\ Image\ Utility.app /Applications/Utilities/System\ Image\ Utility.app
+#Network Utility
+ln -s "/System/Library/CoreServices/Applications/Network Utility.app" "/Applications/Utilities/Network Utility.app"
+
+#Directory Utility
+ln -s "/System/Library/CoreServices/Applications/Directory Utility.app" "/Applications/Utilities/Directory Utility.app"
+
+#Screen Sharing
+ln -s "/System/Library/CoreServices/Applications/Screen Sharing.app" "/Applications/Utilities/Screen Sharing.app"
+
+#RAID Utility
+ln -s "/System/Library/CoreServices/Applications/RAID Utility.app" "/Applications/Utilities/RAID Utility.app"
+
+#Archive Utility
+ln -s "/System/Library/CoreServices/Applications/Archive Utility.app" "/Applications/Utilities/Archive Utility.app"
+
+#About This Mac
+ln -s "/System/Library/CoreServices/Applications/About This Mac.app" "/Applications/Utilities/About This Mac.app"
+
+#Storage Management
+ln -s "/System/Library/CoreServices/Applications/Storage Management.app" "/Applications/Utilities/Storage Management.app"
+
+#Software Update
+ln -s "/System/Library/CoreServices/Software Update.app" "/Applications/Utilities/Software Update.app"
+
+#Ticket Viewer
+ln -s "/System/Library/CoreServices/Ticket Viewer.app" "/Applications/Utilities/Ticket Viewer.app"
+
+#Siri
+ln -s "/System/Library/CoreServices/Siri.app" "/Applications/Utilities/Siri.app"
+
+#Certificate Assistant
+ln -s "/System/Library/CoreServices/Certificate Assistant.app" "/Applications/Utilities/Certificate Assistant.app"
+
+#System Image Utility
+ln -s "/System/Library/CoreServices/Applications/System Image Utility.app" "/Applications/Utilities/System Image Utility.app"
+
+#AirDrop
+ln -s "/System/Library/CoreServices/Finder.app/Contents/Applications/AirDrop.app" "/Applications/Utilities/AirDrop.app"
+
+#All My Files
+ln -s "/System/Library/CoreServices/Finder.app/Contents/Applications/All My Files.app" "/Applications/Utilities/All My Files.app"
+
+#Computer
+ln -s "/System/Library/CoreServices/Finder.app/Contents/Applications/Computer.app" "/Applications/Utilities/Computer.app"
+
+#iCloud Drive
+ln -s "/System/Library/CoreServices/Finder.app/Contents/Applications/iCloud Drive.app" "/Applications/Utilities/iCloud Drive.app"
+
+#Network
+ln -s "/System/Library/CoreServices/Finder.app/Contents/Applications/Network.app" "/Applications/Utilities/Network.app"
+
+#Setup Assistant
+ln -s "/System/Library/CoreServices/Setup Assistant.app" "/Applications/Utilities/Setup Assistant.app"
+
+#Finder
+ln -s "/System/Library/CoreServices/Finder.app" "/Applications/Utilities/Finder.app"
+
+
+
+
+
+
+
